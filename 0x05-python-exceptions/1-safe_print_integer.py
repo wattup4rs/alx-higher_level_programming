bd2be38de7cc8b961bb567aa2a8da67eb5d6e617
wattup4rs <gideonwatt@gmail.com>
2023-08-29 04:39:39 +0000

    python_exceptions

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_interger(value):
+    try:
+        print("{:d}".format(value))
+    except:
+        return (False)
+    else:
+        return (True)
+
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def raise_exception_msg(messages=""):
+    raise NameError(message)
+
