5e49367f653ed4b349fe3fe2f74394c294bba64c
wattup4rs <108527219+wattup4rs@users.noreply.github.com>
2023-10-03 05:13:02 +0100

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    s = ""
-    for i in range(len(str)):
-        if i != n:
-            s = s + str[i]
-    return (s)
-
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-for number in range(99):
-	print("{:d} = 0x{:x}".format(number, number))
-
