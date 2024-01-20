26b226e569919de525c99f29e494d6efd0e9aefd
wattup4rs <gideonwatt@gmail.com>
2024-01-19 18:51:46 -0800

    '

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,9 @@
 Defines a State model.
 Inherits from SQLAlchemy Base and links to the Mysql table states.
 """
-
+from lib2to3.pytree import Base
+from sre_parse import State
+from unicodedata import name
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -19,4 +21,4 @@ class State(Base):
     """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)`
+    name = Column(String(128), nullable=False)
