954c66c5fd0b4b923221dcd5547ec2c5f5612cde
wattup4rs <gideonwatt@gmail.com>
2024-01-26 20:22:31 -0800

    get github commits

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Uses the Github API to display a GitHub ID baseed on given credentials.
+
+Usage: ./10-my_github.py <GitHUB username> <GitHub password>
+"""
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
