dff003a7e0659a6e03ce4dc0ba7f43bec24e6434
wattup4rs <gideonwatt@gmail.com>
2024-03-27 07:49:42 -0700

    python

--- 0-script.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-// a Javascript script that updates the text color
-// of the <header> element to red (#FF0000)
-const header = document.querySelector('header');
-header.style.color = '#FF0000';
--- 6-script.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-// a JavaScript script that updates the text of the <header> element to
-// New Header!!! when the user clicks on DIV#update_header
-const $header = $('header');
-const $updateHeader = $('#update_header');
-$updateHeader.on('click', () => {
-  $header.text('New Header!!!');
-});
